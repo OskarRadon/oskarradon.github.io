0569016d19401034f2baf0515cbda93534102207
Oskar <oskarradonkimball@gmail.com>
2020-10-18 01:43:56 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+// window.addEventListener('load', function () {
 
   var btn = document.getElementById('info-toggle')
   btn.onclick = function() {
@@ -12,4 +12,29 @@ window.addEventListener('load', function () {
       btn.innerHTML = '<b><a href="#">More info?</a></b>'
     }
   }
-});
+// });
+
+var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+    ],
+    d, m, y, now, out_string;
+now = new Date();
+d = now.getDate();
+m = now.getMonth();
+y = now.getFullYear();
+
+
+out_string = months[m] + ' ' + d + ', ' + y;
+
+document.getElementById('date').innerHTML = out_string;
