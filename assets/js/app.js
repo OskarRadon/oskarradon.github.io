1d8cfc8cebf239bd31c834e6bcf7cb6dc29db129
Oskar <oskarradonkimball@gmail.com>
2020-10-19 09:01:36 -0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,28 +13,3 @@
     }
   }
 // });
-
-var months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-    ],
-    d, m, y, now, out_string;
-now = new Date();
-d = now.getDate();
-m = now.getMonth();
-y = now.getFullYear();
-
-
-out_string = months[m] + ' ' + d + ', ' + y;
-
-document.getElementById('date').innerHTML = out_string;
